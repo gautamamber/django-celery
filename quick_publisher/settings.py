8bb3c4430d032614bd6b96cb11387550b64df60b
gautamamber <ambergautam1@gmail.com>
2020-03-19 12:40:44 +0530

    feat():add user model

--- quick_publisher/settings.py
+++ quick_publisher/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'main.User'
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
