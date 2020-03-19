91f53835d94212aeb7ed7be2c6963f7c1b417c0e
gautamamber <ambergautam1@gmail.com>
2020-03-19 12:49:12 +0530

    feat():add first post in publish

--- quick_publisher/urls.py
+++ quick_publisher/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from publish.views import view_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<slug:slug>/', view_post, name='view_post')
 ]
